d1f606fb2b5753386f33b20765fa07b7c8f6d3d5
matthurtado <matthewrosshurtado@gmail.com>
2022-07-05 20:02:17 -0400

    Update spongebobify.py

--- spongebobify.py
+++ spongebobify.py
@@ -8,7 +8,7 @@ from flask import send_file
 
 def find_font_size(text, font, image, target_width_ratio):
     tested_font_size = 100
-    tested_font = ImageFont.truetype("arial.ttf", tested_font_size)
+    tested_font = ImageFont.truetype(font, tested_font_size)
     observed_width, observed_height = get_text_size(text, image, tested_font)
     estimated_font_size = tested_font_size / (observed_width / image.width) * target_width_ratio
     return round(estimated_font_size)
@@ -30,7 +30,7 @@ def spongebobify(text):
 def create_image(text, font, image_path):
     image = Image.open(image_path)
     font_size = find_font_size(text, font, image, 0.5)
-    font = ImageFont.truetype("arial.ttf", font_size)
+    font = ImageFont.truetype(font, font_size)
     # # Call draw Method to add 2D graphics in an image
     I1 = ImageDraw.Draw(image)
     # # Add Text to an image
