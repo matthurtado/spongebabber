49fc854d34ec66f8e228269086aa58d745fbccc7
matthurtado <matthewrosshurtado@gmail.com>
2022-07-08 23:07:27 -0400

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,12 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
+    ON_HEROKU = os.environ.get('ON_HEROKU')
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
+    if(ON_HEROKU):
+        # Have to do this because Heroku is a dumb piece of shit
+        SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
+    else:
+        SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     USERNAME = os.environ.get('USERNAME')
