b2198289fd95af0889e1088df7000d11e421e5f0
matthurtado <matthewrosshurtado@gmail.com>
2022-07-08 22:46:02 -0400

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     USERNAME = os.environ.get('USERNAME')
