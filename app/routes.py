90d5754510c3aa4040e2e1168604a2e90502402f
Seth Anderson <seth@hellonesh.io>
2022-07-10 12:43:05 -0500

    cleanup

--- app/routes.py
+++ app/routes.py
@@ -4,44 +4,65 @@ from app.models import LogRequest, User
 from flask import jsonify, render_template, redirect, url_for, json, request, session
 from config import env
 
+USR_REQ_PER_DAY = 10
+NUM_INITIAL_REQUESTS = 50
+PAGE_DEFAULT = 10
+GOOGLE_CONF_URL = "https://accounts.google.com/.well-known/openid-configuration"
+FONT_URL = "https://github.com/matthurtado/spongebabber/blob/main/app/static/impact.ttf?raw=true"
+MEME_VARS = {
+    "text_to_sponge": "textToSponge",
+    "image": "imageOverride",
+    "text_x_pos": "textXPos",
+    "text_y_pos": "textYPos",
+    "target_width_ratio": "targetWidthRatio",
+    "sponge_the_text": "spongeTheText",
+}
+MEME_DEFAULTS = {
+    "text_to_sponge": None,
+    "image": "https://i.imgur.com/XbXj7M4.jpg",
+    "text_x_pos": 28,
+    "text_y_pos": 280,
+    "target_width_ratio": 0.6,
+    "sponge_the_text": False,
+}
+
 # Database helpers
 def get_or_create(session, model, **kwargs):
     instance = session.query(model).filter_by(**kwargs).first()
     if instance:
         return instance
-    else:
-        instance = model(**kwargs)
-        session.add(instance)
-        return instance
+
+    instance = model(**kwargs)
+    session.add(instance)
+    return instance
 
 
 def get_user_rate_limit():
-    if not session.get("id", None):
-        return "10/day"
-    else:
-        return User.query.filter_by(id=session["id"]).first().requests_per_day
+    return (
+        f"{USR_REQ_PER_DAY}/day"
+        if not session.get("id", None)
+        else User.query.filter_by(id=session["id"]).first().requests_per_day
+    )
 
 
 def get_number_of_requests_today(user_id=None):
     if user_id is None:
-        return 10
-    else:
-        todays_datetime = datetime(
-            datetime.today().year, datetime.today().month, datetime.today().day
-        )
-        number_of_requests = User.query.filter_by(id=user_id).first().requests_per_day
-        number_of_requests_today = (
-            LogRequest.query.filter(LogRequest.timestamp >= todays_datetime)
-            .filter_by(user_id=user_id)
-            .count()
-        )
-        return number_of_requests - number_of_requests_today
+        return USR_REQ_PER_DAY
+
+    today = datetime.today()
+    todays_datetime = datetime(today.year, today.month, today.day)
+    number_of_requests = User.query.filter_by(id=user_id).first().requests_per_day
+    number_of_requests_today = (
+        LogRequest.query.filter(LogRequest.timestamp >= todays_datetime)
+        .filter_by(user_id=user_id)
+        .count()
+    )
+    return number_of_requests - number_of_requests_today
 
 
 # Routes
 @app.route("/")
 def home():
-    print(session)
     return render_template("home.html")
 
 
@@ -53,86 +74,67 @@ def recent():
 
 @app.route("/api/recent")
 @app.route("/api/recent/<int:page>")
-def api_recent(page=None):
-    if page is None:
-        page = 10
+def api_recent(page=PAGE_DEFAULT):
     sponges = LogRequest.query.order_by(LogRequest.timestamp.desc()).limit(page)
     return jsonify(sponges=LogRequest.serialize_list(sponges))
 
 
 @app.route("/spongebobify/", methods=["POST"])
-@app.route("/spongebobify/<textToSponge>", methods=["GET", "POST"])
+@app.route("/spongebobify/<text_to_sponge>", methods=["GET", "POST"])
 @limiter.limit(get_user_rate_limit)
-def spongebobify_there(textToSponge=None):
+def spongebobify_there(text_to_sponge=None):
     content_type = request.headers.get("Content-Type")
-    if content_type == "application/json":
-        # Initialize image settings
-        imageOverride = "https://i.imgur.com/XbXj7M4.jpg"
-        textXPos = 28
-        textYPos = 280
-        targetWidthRatio = 0.6
-        spongeTheText = True
-
-        # Load json data
-        data = json.loads(request.data)
-        textToSponge = data["textToSponge"]
-
-        if data["imageOverride"]:
-            imageOverride = data["imageOverride"]
-        if data["textXPos"]:
-            textXPos = int(data["textXPos"])
-        if data["textYPos"]:
-            textYPos = int(data["textYPos"])
-        if data["targetWidthRatio"]:
-            targetWidthRatio = float(data["targetWidthRatio"])
-        if data["spongeTheText"]:
-            spongeTheText = True
-        else:
-            spongeTheText = False
-
-        # Create the image to return
-        encoded_image = spongebobify.create_image(
-            textToSponge,
-            "https://github.com/matthurtado/spongebabber/blob/main/app/static/impact.ttf?raw=true",
-            imageOverride,
-            textXPos,
-            textYPos,
-            targetWidthRatio,
-            spongeTheText,
-        )
-        if spongeTheText:
-            textToSponge = spongebobify.spongebobify(textToSponge)
-        log_request = LogRequest(
-            text=textToSponge,
-            text_x_pos=textXPos,
-            text_y_pos=textYPos,
-            target_width_ratio=targetWidthRatio,
-            sponge_the_text=spongeTheText,
-            ip_address=request.remote_addr,
-            timestamp=datetime.now(),
-            user_id=session.get("id") or None,
-        )
-        db.session.add(log_request)
-        db.session.commit()
-        return encoded_image.decode("utf-8")
-    else:
+    if content_type != "application/json":
         return "Content-Type not supported!"
+
+    # Initialize image settings
+    my_meme = MEME_DEFAULTS.copy()
+
+    # Load json data
+    data = json.loads(request.data)
+    for key in MEME_VARS:
+        my_meme[key] = data.get(MEME_VARS[key], my_meme[key])
+
+    # Create the image to return
+    text_to_sponge = my_meme.get("text_to_sponge") or text_to_sponge
+    encoded_image = spongebobify.create_meme(
+        text_to_sponge,
+        FONT_URL,
+        my_meme.get("image"),
+        my_meme.get("text_x_pos"),
+        my_meme.get("text_y_pos"),
+        float(my_meme.get("target_width_ratio")),
+        my_meme.get("sponge_the_text"),
+    )
+    if my_meme.get("sponge_the_text"):
+        text_to_sponge = spongebobify.spongebobify(text_to_sponge)
+    log_request = LogRequest(
+        text=text_to_sponge,
+        text_x_pos=my_meme.get("text_x_pos"),
+        text_y_pos=my_meme.get("text_y_pos"),
+        target_width_ratio=float(my_meme.get("target_width_ratio")),
+        sponge_the_text=my_meme.get("sponge_the_text"),
+        ip_address=request.remote_addr,
+        timestamp=datetime.now(),
+        user_id=session.get("id") or None,
+    )
+    db.session.add(log_request)
+    db.session.commit()
+    return encoded_image.decode("utf-8")
 
 
 @app.route("/google/")
 def google():
-    CONF_URL = "https://accounts.google.com/.well-known/openid-configuration"
     oauth.register(
         name="google",
         client_id=env.GOOGLE_CLIENT_ID,
         client_secret=env.GOOGLE_CLIENT_SECRET,
-        server_metadata_url=CONF_URL,
+        server_metadata_url=GOOGLE_CONF_URL,
         client_kwargs={"scope": "openid email profile"},
     )
 
     # Redirect to google_auth function
     redirect_uri = url_for("google_auth", _external=True)
-    print(redirect_uri)
     return oauth.google.authorize_redirect(redirect_uri)
 
 
@@ -145,17 +147,15 @@ def google_auth():
 
         user_db = get_or_create(db.session, User, email=user["email"])
         if user_db.last_login is None:
-            user_db.email = user["email"]
-            user_db.name = user["name"]
-            user_db.last_login = datetime.now()
-            user_db.num_of_requests = 50
-            db.session.commit()
-        else:
-            user_db.last_login = datetime.now()
-            db.session.commit()
+            user_db.email, user_db.name = user["email"], user["name"]
+            user_db.num_of_requests = NUM_INITIAL_REQUESTS
+        user_db.last_login = datetime.now()
+        db.session.commit()
+
         session["is_admin"] = user_db.is_admin or False
         session["id"] = user_db.id
         session["requests_per_day"] = user_db.requests_per_day
+
     return redirect("/")
 
 
@@ -167,11 +167,9 @@ def logout():
 
 @app.route("/account/")
 def account():
-    if "user" in session:
-        user = session["user"]
-        session["remaining_requests_per_day"] = get_number_of_requests_today(
-            session["id"]
-        )
-        return render_template("account.html", user=user)
-    else:
+    if "user" not in session:
         return redirect("/")
+
+    user = session["user"]
+    session["remaining_requests_per_day"] = get_number_of_requests_today(session["id"])
+    return render_template("account.html", user=user)
--- app/spongebobify.py
+++ app/spongebobify.py
@@ -7,7 +7,7 @@ from PIL import Image, ImageDraw, ImageFont, ImageSequence
 def find_font_size(text, font, image, target_width_ratio):
     tested_font_size = 100
     tested_font = ImageFont.truetype(urlopen(font), tested_font_size)
-    observed_width, observed_height = get_text_size(text, image, tested_font)
+    observed_width, _ = get_text_size(text, image, tested_font)
     estimated_font_size = (
         tested_font_size / (observed_width / image.width) * target_width_ratio
     )
@@ -23,61 +23,18 @@ def get_text_size(text, image, font):
 def spongebobify(text):
     res = ""
     for count, value in enumerate(text):
-        if count % 2 == 0:
-            res += value.upper()
-        else:
-            res += value.lower()
+        res += value.upper() if count % 2 == 0 else value.lower()
     return res
 
 
-def create_image(
-    text, font, image_path, text_x_pos, text_y_pos, target_width_ratio, sponge_the_text
-):
-    image = Image.open(urlopen(image_path))
-    font_size = find_font_size(text, font, image, target_width_ratio)
-    font = ImageFont.truetype(urlopen(font), font_size)
-    text_on_image = spongebobify(text) if sponge_the_text else text
-    img_io = BytesIO()
-    if ".gif" in image_path:
-        # A list of the frames to be outputted
-        frames = []
-        # Loop over each frame in the animated image
-        for frame in ImageSequence.Iterator(image):
-            # Draw the text on the frame
-            d = ImageDraw.Draw(frame)
-            d.text(
-                (text_x_pos, text_y_pos),
-                text_on_image,
-                font=font,
-                fill=(255, 255, 255),
-                stroke_fill=(0, 0, 0),
-                stroke_width=3,
-            )
-            del d
-
-            # However, 'frame' is still the animated image with many frames
-            # It has simply been seeked to a later frame
-            # For our list of frames, we only want the current frame
-
-            # Saving the image without 'save_all' will turn it into a single frame image, and we can then re-open it
-            # To be efficient, we will save it to a stream, rather than to file
-            b = BytesIO()
-            frame.save(b, format="GIF")
-            frame = Image.open(b)
-
-            # Then append the single frame image to a list of frames
-            frames.append(frame)
-
-        # Save the frames as a new image
-        frames[0].save(img_io, save_all=True, append_images=frames[1:], format="GIF")
-        img_io.seek(0)
-        # do gif work
-        return base64.b64encode(img_io.getvalue())
-    else:
-        # # Call draw Method to add 2D graphics in an image
-        I1 = ImageDraw.Draw(image)
-        # # Add Text to an image
-        I1.text(
+def create_gif(image, text_x_pos, text_y_pos, text_on_image, font, img_io):
+    # A list of the frames to be outputted
+    frames = []
+    # Loop over each frame in the animated image
+    for frame in ImageSequence.Iterator(image):
+        # Draw the text on the frame
+        d = ImageDraw.Draw(frame)
+        d.text(
             (text_x_pos, text_y_pos),
             text_on_image,
             font=font,
@@ -85,6 +42,55 @@ def create_image(
             stroke_fill=(0, 0, 0),
             stroke_width=3,
         )
-        image.save(img_io, format="JPEG")
-        img_io.seek(0)
-        return base64.b64encode(img_io.getvalue())
+        del d
+
+        # However, 'frame' is still the animated image with many frames
+        # It has simply been seeked to a later frame
+        # For our list of frames, we only want the current frame
+
+        # Saving the image without 'save_all' will turn it into a single frame image, and we can then re-open it
+        # To be efficient, we will save it to a stream, rather than to file
+        b = BytesIO()
+        frame.save(b, format="GIF")
+        frame = Image.open(b)
+
+        # Then append the single frame image to a list of frames
+        frames.append(frame)
+
+    # Save the frames as a new image
+    frames[0].save(img_io, save_all=True, append_images=frames[1:], format="GIF")
+    img_io.seek(0)
+    # do gif work
+    return base64.b64encode(img_io.getvalue())
+
+
+def create_image(image, text_x_pos, text_y_pos, text_on_image, font, img_io):
+    # # Call draw Method to add 2D graphics in an image
+    I1 = ImageDraw.Draw(image)
+    # # Add Text to an image
+    I1.text(
+        (text_x_pos, text_y_pos),
+        text_on_image,
+        font=font,
+        fill=(255, 255, 255),
+        stroke_fill=(0, 0, 0),
+        stroke_width=3,
+    )
+    image.save(img_io, format="JPEG")
+    img_io.seek(0)
+    return base64.b64encode(img_io.getvalue())
+
+
+def create_meme(
+    text, font, image_path, text_x_pos, text_y_pos, target_width_ratio, sponge_the_text
+):
+    image = Image.open(urlopen(image_path))
+    font_size = find_font_size(text, font, image, target_width_ratio)
+    font = ImageFont.truetype(urlopen(font), font_size)
+    text_on_image = spongebobify(text) if sponge_the_text else text
+    img_io = BytesIO()
+    return (
+        create_gif(image, text_x_pos, text_y_pos, text_on_image, font, img_io)
+        if ".gif" in image_path
+        else create_image(image, text_x_pos, text_y_pos, text_on_image, font, img_io)
+    )
