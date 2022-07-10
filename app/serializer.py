bdac0ba7cd2cf7d9e7ccde6e6dc36f3b0a05a5fe
Seth Anderson <seth@hellonesh.io>
2022-07-10 11:45:32 -0500

    'black' python formatting

--- app/serializer.py
+++ app/serializer.py
@@ -1,7 +1,7 @@
 from sqlalchemy.inspection import inspect
 
-class Serializer(object):
 
+class Serializer(object):
     def serialize(self):
         return {c: getattr(self, c) for c in inspect(self).attrs.keys()}
 
--- app/spongebobify.py
+++ app/spongebobify.py
@@ -3,41 +3,56 @@ from io import BytesIO
 from urllib.request import urlopen
 from PIL import Image, ImageDraw, ImageFont, ImageSequence
 
+
 def find_font_size(text, font, image, target_width_ratio):
     tested_font_size = 100
     tested_font = ImageFont.truetype(urlopen(font), tested_font_size)
     observed_width, observed_height = get_text_size(text, image, tested_font)
-    estimated_font_size = tested_font_size / (observed_width / image.width) * target_width_ratio
+    estimated_font_size = (
+        tested_font_size / (observed_width / image.width) * target_width_ratio
+    )
     return round(estimated_font_size)
 
+
 def get_text_size(text, image, font):
-    im = Image.new('RGB', (image.width, image.height))
+    im = Image.new("RGB", (image.width, image.height))
     draw = ImageDraw.Draw(im)
     return draw.textsize(text, font)
+
 
 def spongebobify(text):
     res = ""
     for count, value in enumerate(text):
-        if(count % 2 == 0):
+        if count % 2 == 0:
             res += value.upper()
         else:
             res += value.lower()
     return res
 
-def create_image(text, font, image_path, text_x_pos, text_y_pos, target_width_ratio, sponge_the_text):
+
+def create_image(
+    text, font, image_path, text_x_pos, text_y_pos, target_width_ratio, sponge_the_text
+):
     image = Image.open(urlopen(image_path))
     font_size = find_font_size(text, font, image, target_width_ratio)
     font = ImageFont.truetype(urlopen(font), font_size)
     text_on_image = spongebobify(text) if sponge_the_text else text
     img_io = BytesIO()
-    if(".gif" in image_path):
+    if ".gif" in image_path:
         # A list of the frames to be outputted
         frames = []
         # Loop over each frame in the animated image
         for frame in ImageSequence.Iterator(image):
             # Draw the text on the frame
             d = ImageDraw.Draw(frame)
-            d.text((text_x_pos, text_y_pos), text_on_image, font=font, fill=(255, 255, 255), stroke_fill=(0, 0, 0), stroke_width=3)
+            d.text(
+                (text_x_pos, text_y_pos),
+                text_on_image,
+                font=font,
+                fill=(255, 255, 255),
+                stroke_fill=(0, 0, 0),
+                stroke_width=3,
+            )
             del d
 
             # However, 'frame' is still the animated image with many frames
@@ -52,7 +67,7 @@ def create_image(text, font, image_path, text_x_pos, text_y_pos, target_width_ra
 
             # Then append the single frame image to a list of frames
             frames.append(frame)
-        
+
         # Save the frames as a new image
         frames[0].save(img_io, save_all=True, append_images=frames[1:], format="GIF")
         img_io.seek(0)
@@ -62,7 +77,14 @@ def create_image(text, font, image_path, text_x_pos, text_y_pos, target_width_ra
         # # Call draw Method to add 2D graphics in an image
         I1 = ImageDraw.Draw(image)
         # # Add Text to an image
-        I1.text((text_x_pos, text_y_pos), text_on_image, font=font, fill=(255, 255, 255), stroke_fill=(0, 0, 0), stroke_width=3)
-        image.save(img_io, format='JPEG')
+        I1.text(
+            (text_x_pos, text_y_pos),
+            text_on_image,
+            font=font,
+            fill=(255, 255, 255),
+            stroke_fill=(0, 0, 0),
+            stroke_width=3,
+        )
+        image.save(img_io, format="JPEG")
         img_io.seek(0)
         return base64.b64encode(img_io.getvalue())
