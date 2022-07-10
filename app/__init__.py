f275e23aec8c2563f0a7453b0ec022734a5cf843
Seth Anderson <seth@hellonesh.io>
2022-07-10 11:43:52 -0500

    local env file and env vars check

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,6 @@
-import os
 from flask import Flask, redirect
 from requests import session
-from config import Config
+from config import env
 from flask_bootstrap import Bootstrap
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
@@ -10,15 +9,15 @@ from flask_limiter.util import get_remote_address
 from authlib.integrations.flask_client import OAuth
 
 app = Flask(__name__)
-app.config.from_object(Config)
+app.config.from_object(env)
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
-migrate = Migrate(app,db)
+migrate = Migrate(app, db)
 limiter = Limiter(app, key_func=get_remote_address)
 oauth = OAuth(app)
 
 from app import routes, models
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(threaded=True, port=5000)
-    session['is_admin'] = False
+    session["is_admin"] = False
--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from app import app, db, spongebobify, limiter, oauth
 from app.models import LogRequest, User
 from flask import jsonify, render_template, redirect, url_for, json, request, session
-from config import Config
+from config import env
 
 # Database helpers
 def get_or_create(session, model, **kwargs):
@@ -14,20 +14,29 @@ def get_or_create(session, model, **kwargs):
         session.add(instance)
         return instance
 
+
 def get_user_rate_limit():
-    if(session['id'] is None):
+    if not session.get("id", None):
         return "10/day"
     else:
-        return User.query.filter_by(id=session['id']).first().requests_per_day
+        return User.query.filter_by(id=session["id"]).first().requests_per_day
 
-def get_number_of_requests_today(user_id = None):
-    if(user_id is None):
+
+def get_number_of_requests_today(user_id=None):
+    if user_id is None:
         return 10
     else:
-        todays_datetime = datetime(datetime.today().year, datetime.today().month, datetime.today().day)
+        todays_datetime = datetime(
+            datetime.today().year, datetime.today().month, datetime.today().day
+        )
         number_of_requests = User.query.filter_by(id=user_id).first().requests_per_day
-        number_of_requests_today = LogRequest.query.filter(LogRequest.timestamp >= todays_datetime).filter_by(user_id=user_id).count()
+        number_of_requests_today = (
+            LogRequest.query.filter(LogRequest.timestamp >= todays_datetime)
+            .filter_by(user_id=user_id)
+            .count()
+        )
         return number_of_requests - number_of_requests_today
+
 
 # Routes
 @app.route("/")
@@ -81,7 +90,7 @@ def spongebobify_there(textToSponge=None):
         else:
             spongeTheText = False
 
-        #Create the image to return
+        # Create the image to return
         encoded_image = spongebobify.create_image(
             textToSponge,
             "https://github.com/matthurtado/spongebabber/blob/main/app/static/impact.ttf?raw=true",
@@ -101,7 +110,7 @@ def spongebobify_there(textToSponge=None):
             sponge_the_text=spongeTheText,
             ip_address=request.remote_addr,
             timestamp=datetime.now(),
-            user_id=session.get('id') or None
+            user_id=session.get("id") or None,
         )
         db.session.add(log_request)
         db.session.commit()
@@ -109,59 +118,60 @@ def spongebobify_there(textToSponge=None):
     else:
         return "Content-Type not supported!"
 
-@app.route('/google/')
-def google():
-    GOOGLE_CLIENT_ID = Config.GOOGLE_CLIENT_ID
-    GOOGLE_CLIENT_SECRET = Config.GOOGLE_CLIENT_SECRET
 
-    CONF_URL = 'https://accounts.google.com/.well-known/openid-configuration'
+@app.route("/google/")
+def google():
+    CONF_URL = "https://accounts.google.com/.well-known/openid-configuration"
     oauth.register(
-        name='google',
-        client_id=GOOGLE_CLIENT_ID,
-        client_secret=GOOGLE_CLIENT_SECRET,
+        name="google",
+        client_id=env.GOOGLE_CLIENT_ID,
+        client_secret=env.GOOGLE_CLIENT_SECRET,
         server_metadata_url=CONF_URL,
-        client_kwargs={
-            'scope': 'openid email profile'
-        }
+        client_kwargs={"scope": "openid email profile"},
     )
 
     # Redirect to google_auth function
-    redirect_uri = url_for('google_auth', _external=True)
+    redirect_uri = url_for("google_auth", _external=True)
     print(redirect_uri)
     return oauth.google.authorize_redirect(redirect_uri)
 
-@app.route('/google/auth/')
+
+@app.route("/google/auth/")
 def google_auth():
     token = oauth.google.authorize_access_token()
-    user = token.get('userinfo')
+    user = token.get("userinfo")
     if user:
-        session['user'] = user
-        
-        user_db = get_or_create(db.session, User, email=user['email'])
-        if(user_db.last_login is None):
-            user_db.email = user['email']
-            user_db.name = user['name']
+        session["user"] = user
+
+        user_db = get_or_create(db.session, User, email=user["email"])
+        if user_db.last_login is None:
+            user_db.email = user["email"]
+            user_db.name = user["name"]
             user_db.last_login = datetime.now()
             user_db.num_of_requests = 50
             db.session.commit()
         else:
             user_db.last_login = datetime.now()
             db.session.commit()
-        session['is_admin'] = user_db.is_admin or False
-        session['id'] = user_db.id
-        session['requests_per_day'] = user_db.requests_per_day
-    return redirect('/')
+        session["is_admin"] = user_db.is_admin or False
+        session["id"] = user_db.id
+        session["requests_per_day"] = user_db.requests_per_day
+    return redirect("/")
 
-@app.route('/logout/')
+
+@app.route("/logout/")
 def logout():
-    session.pop('user', None)
-    return redirect('/')
+    session.pop("user", None)
+    return redirect("/")
 
-@app.route('/account/')
+
+@app.route("/account/")
 def account():
-    if 'user' in session:
-        user = session['user']
-        session['remaining_requests_per_day'] = get_number_of_requests_today(session['id'])
-        return render_template('account.html', user=user)
+    if "user" in session:
+        user = session["user"]
+        session["remaining_requests_per_day"] = get_number_of_requests_today(
+            session["id"]
+        )
+        return render_template("account.html", user=user)
     else:
-        return redirect('/')
+        return redirect("/")
